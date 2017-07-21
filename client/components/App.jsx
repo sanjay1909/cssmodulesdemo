3397dacb2034e97308dcae04c11db17aa452a513
Anbalagan <sanbalagan@mabossanbala-m1.corp.local>
2017-07-21 12:23:45 -0400

    App initiated for Demo testing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,17 @@
  ./client/components/App.jsx
  */
 import React from 'react';
+import styles from './style.module.scss';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div style={{textAlign: 'center'}}>
-                <h1>CSS Modules Test</h1>
+            <div>
+                <h1 style={{textAlign: 'center'}}>CSS Modules Test</h1>
+                <div className={styles.blockquote}>
+                    <span>CSS modules compilee generate the classname as </span>
+                    {styles.blockquote}
+                </div>
             </div>);
     }
 }
