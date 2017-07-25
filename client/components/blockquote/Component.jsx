e85e1fa6fef7c14a753bae165afa3fd369365267
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2017-07-25 12:59:27 -0400

    Better description added

--- client/components/blockquote/Component.jsx
+++ client/components/blockquote/Component.jsx
@@ -11,12 +11,12 @@ export default class Blockquote extends React.Component {
         if(this.props.isLocal){
             blockClass = styles.blockquote;
             description = (<ol className="olc">
-                                <li><u>blockquote.css</u> loaded by <b>CSS-Loader</b> into <u>Blockquote.jsx</u> by Webpack</li>
-                                <li>Since <u>module option set to true in webpack config</u>,<b>CSS-Loader</b> creates js object in <u>Blockquote.jsx</u> </li>
-                                <li><u> key is </u> blockquote</li>
-                                <li><u> value is </u> {blockClass}</li>
-                                <li><u> {blockClass}</u> css value is injected into <u>head tag</u> as <u>style tag</u> by <b>style-loader</b></li>
-                            </ol>);
+                <li><u><b>CSS-Loader: </b></u>helps webpack to <i>import</i> <u>blockquote.css</u> in <u>Blockquote.jsx</u></li>
+                <li><b>CSS-Loader: </b> (option = module: true),Creates js object in <u>Blockquote.jsx</u> </li>
+                <li><u>key:</u> blockquote</li>
+                <li><u>value:</u> {blockClass}</li>
+                <li><u><b>Style-Loader: </b></u> helps webpack to <i>inject</i> <u>style tag</u> with its css value(.{blockClass}) in <u>head tag</u></li>
+            </ol>);
         }else{
             blockClass = "blockquote";
             description = (<ol className="olc">
--- client/components/para/Component.jsx
+++ client/components/para/Component.jsx
@@ -9,12 +9,12 @@ export default class Para extends React.Component {
         let description;
         if(this.props.isLocal){
             blockClass = styles.para;
-            description = (<ol>
-                <li><u>para.css</u> loaded by <b>CSS-Loader</b> into <u>Para.jsx</u></li>
-                <li>Since <u>module option set to true in webpack config</u>,<b>CSS-Loader</b> creates js object in <u>Para.jsx</u> </li>
-                <li><u> key is </u> para</li>
-                <li><u> value is </u> {blockClass}</li>
-                <li><u> {blockClass}</u> css value is injected into <u>head tag</u> as <u>style tag</u> by <b>style-loader</b></li>
+            description = (<ol className="olc">
+                <li><u><b>CSS-Loader: </b></u>helps webpack to <i>import</i> <u>para.css</u> in <u>Para.jsx</u></li>
+                <li><b>CSS-Loader: </b> (option = module: true),Creates js object in <u>Para.jsx</u> </li>
+                <li><u>key:</u> para</li>
+                <li><u>value:</u> {blockClass}</li>
+                <li><u><b>Style-Loader: </b></u> helps webpack to <i>inject</i> <u>style tag</u> with its css value(.{blockClass}) in <u>head tag</u></li>
             </ol>);
         }else{
             blockClass = "para";
